d76464bc58e45ae776d73ec146bb46d428799307
Josef Diago <josef.diago@gmail.com>
2021-02-16 21:10:04 -0500

    Update messages_controller_test.rb

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -9,4 +9,21 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
         as: :json
     end
   end
+
+  test "should get a message" do
+    get \
+      messages_url,
+      params: { receiver: 'alice', sender: 'bob', last_30: 't' },
+      as: :json
+
+    assert_response :success
+  end
+
+  test "should require receiver" do
+    get \
+      messages_url,
+      as: :json
+
+    assert_response :bad_request
+  end
 end
