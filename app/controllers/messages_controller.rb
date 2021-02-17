1c2080e04faa068a8b2a4ab5e8d30b7938a93bcd
Josef Diago <josef.diago@gmail.com>
2021-02-16 20:52:46 -0500

    adds index action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,24 @@
 class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    if params[:receiver].blank?
+      render json: { receiver: "can't be blank" }, status: :bad_request
+      return
+    end
+
+    @messages = Message.where(receiver: params[:receiver]).
+      order(created_at: :desc)
+
+    @messages = @messages.where(sender: params[:sender]) if params[:sender]
+
+    @messages = if params[:last_30] == 't'
+      @messages.where('created_at > ?', 30.days.ago)
+    else
+      @messages.limit(100)
+    end
+  end
+
   def create
     @message = Message.new message_params
 
